0c762d0755c33b99bd6c500028d4b60d7c2db067
Aliyan Haider <changezialiyan192@gmail.com>
2026-04-07 16:23:50 +0500

    automated the passcode dialogue box, and added comments for meeting id, and meeting passcode.

--- zoom_automation.py
+++ zoom_automation.py
@@ -15,7 +15,17 @@ audio = child_win.JoinMeeting.child_window(title="Don't connect to audio", contr
 video = child_win.JoinMeeting.child_window(title="Turn off my video", control_type="CheckBox")
 join = child_win.JoinMeeting.child_window(title="Join", control_type="Button")
 
-meeting_id.type_keys("7743816469")
+# enter your meeting id below.
+meeting_id.type_keys("")
 audio.click_input()
 video.click_input()
 join.click_input()
+
+passcode_dialogue = Application(backend="uia").connect(title="Enter meeting passcode", timeout=5)
+
+passcode_box = passcode_dialogue.EnterMeetngPasscode.child_window(title="Meeting passcode", control_type="Edit")
+join_2 = passcode_dialogue.EnterMeetingPasscode.child_window(title="Join meeting", control_type="Button")
+
+# enter your meeting passcode below.
+passcode_box.type_keys("")
+join_2.click_input()
